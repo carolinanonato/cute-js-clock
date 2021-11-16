8383eccbfbb133c66c7fca5ce3127dae3b564827
carolinanonato <csnonato@gmail.comgit config --global user.email>
2021-11-16 15:00:14 -0500

    Get the current hour, minute and second with Date()

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 let hrPosition = 20;
-let minPosition = 130;
+let minPosition = 230;
 let secPosition = 267;
+
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
